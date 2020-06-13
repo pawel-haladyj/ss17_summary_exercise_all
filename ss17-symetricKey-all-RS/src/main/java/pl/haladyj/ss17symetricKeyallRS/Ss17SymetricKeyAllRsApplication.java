49a9ed944d0d6c6955f9027910801c928f2c47d4
Paweł Haładyj <pawel@haladyj.pl>
2020-06-13 17:40:19 +0200

    initial commit

---
+++ ss17-symetricKey-all-RS/src/main/java/pl/haladyj/ss17symetricKeyallRS/Ss17SymetricKeyAllRsApplication.java
@@ -0,0 +1,13 @@
+package pl.haladyj.ss17symetricKeyallRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss17SymetricKeyAllRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ss17SymetricKeyAllRsApplication.class, args);
+	}
+
+}
---
+++ ss17-symetricKey-all-RS/src/test/java/pl/haladyj/ss17symetricKeyallRS/Ss17SymetricKeyAllRsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.ss17symetricKeyallRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss17SymetricKeyAllRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
